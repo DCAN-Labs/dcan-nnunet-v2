76e055954327ad4e5ab2e6e2ebcc349c63792ece
lundq163 <lundq163@umn.edu>
2025-10-17 16:59:11 -0500

    add resize_images script for resizing images with specified dimensions

---
+++ dcan/img_preproc/resize_images.py
@@ -0,0 +1,57 @@
+"""
+Resize Images.
+
+Usage:
+    resize_images <input_folder> <output_folder> [--model {infant,lifespan}]
+    resize_images -h | --help
+
+Options:
+    -h --help    Show this help message.
+    --model      Choose 'infant' for (182,218,182) or 'lifespan' for (256,256,256). Default: infant.
+"""
+
+import argparse
+import os
+import shutil
+from os.path import isfile, join
+
+import nilearn
+from nilearn import image
+from tqdm import tqdm
+
+
+def resize_images(input_folder, output_folder, model='infant'):
+        only_files = [f for f in os.listdir(input_folder) if isfile(join(input_folder, f))]
+        if model == 'lifespan':
+                desired_shape = (256, 256, 256)
+        else:
+                desired_shape = (182, 218, 182)
+        for f in tqdm(only_files):
+                file_path = join(input_folder, f)
+                smoothed_img = image.load_img(file_path)
+                header = smoothed_img.header
+                data_shape = header.get_data_shape()
+                if data_shape[:3] != desired_shape:
+                        print(f'Resizing to {desired_shape}: {f}')
+                        if f.endswith('_aseg.nii.gz'):
+                                interpolation = 'nearest'
+                        else:
+                                interpolation = 'continuous'
+                        target_affine = smoothed_img.affine
+                        resampled = nilearn.image.resample_img(smoothed_img, target_shape=desired_shape, target_affine=target_affine, interpolation=interpolation, force_resample=True, copy_header=True)
+                        resampled.to_filename(os.path.join(output_folder, f))
+                else:
+                        shutil.copy(file_path, output_folder)
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(
+                prog='resize_images',
+                description='Resizes all images in a folder to a chosen target shape.',
+                epilog='Contact lundq163 if you have any questions or run into any problems.')
+        parser.add_argument('input_folder')
+        parser.add_argument('output_folder')
+        parser.add_argument('--model', choices=['infant', 'lifespan'], default='infant',
+                                                help="Choose 'infant' for (182,218,182) or 'lifespan' for (256,256,256). Default: infant")
+        args = parser.parse_args()
+        resize_images(args.input_folder, args.output_folder, model=args.model)
